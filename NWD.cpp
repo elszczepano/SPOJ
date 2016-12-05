73764f251eba3793e909b6d73afe66dea1f87126
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2016-12-05 18:26:11 +0100

    Add files via upload

---
+++ NWD.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int NWD(int a, int b)
+{
+    while(a!=b)
+       if(a>b)
+           a-=b;
+       else
+           b-=a;
+    return a;
+}
+int t;
+int main()
+{
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+        int a, b;
+        cin>>a>>b;
+        cout<< NWD(a,b)<<endl;
+    }
+
+    return 0;
+}
