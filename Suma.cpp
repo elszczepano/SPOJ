9c392c0171ef3ac21ac45a62a4a6c5902e49e263
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2016-12-06 18:35:39 +0100

    Add files via upload

---
+++ Suma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int a,b;
+
+int main()
+{
+    for(int i=0; i<10; i++)
+    {
+        cin>>a;
+        b+=a;
+        cout<<b<<endl;
+    }
+    return 0;
+}
