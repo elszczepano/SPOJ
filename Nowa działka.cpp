335abcf8ba2e461aa6d0a681512f9ce3dc435c37
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2016-12-12 16:43:21 +0100

    Add files via upload

---
+++ Nowa działka.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int a,D;
+
+int main()
+{
+    cin>>D;
+    for(int i=0; i<D; i++)
+    {
+        cin>>a;
+        cout<<a*a<<endl;
+    }
+    return 0;
+}
