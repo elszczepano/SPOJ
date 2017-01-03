ebe5a357544b6dbadfc7f8c90bad436a76d27a0e
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-01-03 22:11:17 +0100

    Add files via upload

---
+++ TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int n;
+int main()
+{
+    while(cin>>n)
+    {
+    if(n!=42) cout<<n<<endl;
+    else exit(0);
+    }
+    return 0;
+}
