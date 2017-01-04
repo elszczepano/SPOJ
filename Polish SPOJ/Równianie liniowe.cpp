4a378cb64a9e32ee86c0407f21c8a78c8f0ec834
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-01-04 18:08:41 +0100

    Add files via upload

---
+++ Polish SPOJ/Równianie liniowe.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+double a,b,c,x;
+int d;
+
+int main()
+{
+    while(cin>>a>>b>>c)
+    {
+
+    x=(c-b)/a;
+    if(a==0&&(c-b)!=0) cout<<"BR";
+    else if(a==0&&(c-b)==0) cout<<"NWR";
+    else
+    {
+    d=x;
+    if(d==x) cout<<setprecision(4)<<x<<"."<<fmod(x,x)<<fmod(x,x);
+    else  cout<<setprecision(4)<<x;
+    }
+    }
+    return 0;
+}
