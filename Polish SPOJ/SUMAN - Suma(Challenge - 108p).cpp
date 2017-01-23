32b4250b99ec3185b370cad885b2e7dc6e0f4aa4
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-01-23 15:25:58 +0100

    Update and rename SUMAN - Suma(Challenge - 111p).cpp to SUMAN - Suma(Challenge - 108p).cpp

---
+++ Polish SPOJ/SUMAN - Suma(Challenge - 108p).cpp
@@ -0,0 +1,2 @@
+#include<iostream>
+int n,s;int main(){while(std::cin>>n){while(n>0){s+=n;n--;}std::cout<<s<<std::endl;s=0;}}
--- Polish SPOJ/SUMAN - Suma(Challenge - 111p).cpp
+++
@@ -1,4 +0,0 @@
-#include<iostream>
-int n,s;
-int main()
-{while(std::cin>>n){while(n>0){s+=n;n--;}std::cout<<s<<std::endl;s=0;}}
